c9829d0c486627345a14bf19026b1081f6a18aec
LeonardoLopez <g.lelopez1@gmail.com>
2015-03-22 12:42:20 +0100

    no message

---
+++ Assets/BallController1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController1 : MonoBehaviour {
+	
+	public float force = 9.8f;
+	
+	void FixedUpdate () {
+		Vector3 dir = new Vector3(Input.acceleration.x, Input.acceleration.y, 0.0F);
+		Physics.gravity = dir * force;
+	}
+}
---
+++ Assets/Scripts/BallController.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var movementSpeed_x : float = 0;
+var movementSpeed_y : float = 0;
+var speed : float = 20;
+
+function Start () {
+
+}
+
+function FixedUpdate () {
+	//movementSpeed_x = Input.acceleration.x * speed;
+	//movementSpeed_y = Input.acceleration.y * speed;
+    //GetComponent.<Rigidbody2D>().velocity = new Vector2(movementSpeed_x, movementSpeed_y);
+    Physics.gravity = new Vector3(Input.acceleration.x, Input.acceleration.y, 0);
+}
